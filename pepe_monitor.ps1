85ee641ff91169ac9ee66b4425f11afdfe78e378
Anthony A Davis <35675472+anthonyadavisii@users.noreply.github.com>
2019-02-16 08:13:35 -0600

    Pepe Monitor V1

---
+++ pepe_monitor.ps1
@@ -0,0 +1,98 @@
+<#
+
+__________████████_____██████
+_________█░░░░░░░░██_██░░░░░░█
+________█░░░░░░░░░░░█░░░░░░░░░█
+_______█░░░░░░░███░░░█░░░░░░░░░█
+_______█░░░░███░░░███░█░░░████░█
+______█░░░██░░░░░░░░███░██░░░░██
+_____█░░░░░░░░░░░░░░░░░█░░░░░░░░███
+____█░░░░░░░░░░░░░██████░░░░░████░░█
+____█░░░░░░░░░█████░░░████░░██░░██░░█
+___██░░░░░░░███░░░░░░░░░░█░░░░░░░░███
+__█░░░░░░░░░░░░░░█████████░░█████████
+_█░░░░░░░░░░█████_████___████_█████___█
+_█░░░░░░░░░░█______█_███__█_____███_█___█
+█░░░░░░░░░░░░█___████_████____██_██████
+░░░░░░░░░░░░░█████████░░░████████░░░█
+░░░░░░░░░░░░░░░░█░░░░░█░░░░░░░░░░░░█
+░░░░░░░░░░░░░░░░░░░░██░░░░█░░░░░░██
+░░░░░░░░░░░░░░░░░░██░░░░░░░███████
+░░░░░░░░░░░░░░░░██░░░░░░░░░░█░░░░░█
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+░░░░░░░░░░░█████████░░░░░░░░░░░░░░██
+░░░░░░░░░░█▒▒▒▒▒▒▒▒███████████████▒▒█
+░░░░░░░░░█▒▒███████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+░░░░░░░░░█▒▒▒▒▒▒▒▒▒█████████████████
+░░░░░░░░░░████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+░░░░░░░░░░░░░░░░░░██████████████████
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█
+██░░░░░░░░░░░░░░░░░░░░░░░░░░░██
+▓██░░░░░░░░░░░░░░░░░░░░░░░░██
+▓▓▓███░░░░░░░░░░░░░░░░░░░░█
+▓▓▓▓▓▓███░░░░░░░░░░░░░░░██
+▓▓▓▓▓▓▓▓▓███████████████▓▓█
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█
+
+Name: PepeCoin Monitor
+Author:  Anthony "Alex" Davis II
+Email:   v7davisa@gmail.com
+Website: https://github.com/anthonyadavisii
+Purpose: To monitor PepeCoin / Memetic wallet debug.log and send smtp alerts for ERROR entries allowing 3 hours remediation period before additional alerts.
+Date: 15th Feb 2019
+Version: 1.0
+#>
+
+<#
+.Synopsis
+   Script to send email using Powershell via SendGrid account    
+.DESCRIPTION
+   Script to send email using Powershell via SendGrid account 
+ 
+.NOTES
+    Author:  Deepak Vishwakarma (deep)
+    Email:   Deepitpro@outlook.com
+    Purpose: To send email using PowerShell via SendGrid account (https://sendgrid.com/blog/sendgrid-in-the-cloud-with-microsoft/)
+    Date: 26th Aug 2017
+    Version: 1.0
+    
+#>
+
+function Send-TestEmail{
+    [cmdletbinding()]
+    Param (
+    $To,
+    $Cc,
+    $body
+  
+   )
+
+   Process{
+
+        #Provide SendGrid user name, if you are using Microsoft azure you will find the same from the portal   
+        $sendgridusername = "apikey"
+        #Enter the send grid password Note: this is not recommended for production. In production, the password should be encrypted
+        $SecurePassword=ConvertTo-SecureString $Env:SENDGRID –asplaintext –force 
+        $cred = New-Object System.Management.Automation.PsCredential($sendgridusername,$SecurePassword)
+        $sub="PepeCoin Error"
+        $From = "testsendgrid@mytestdomain.com"
+        Send-MailMessage -From $From -To $To -Cc $Cc -Subject $sub -Body $body -Priority High -SmtpServer "smtp.sendgrid.net" -Credential $cred -UseSsl -Port 587 -BodyAsHtml
+   }
+}
+
+$VerbosePreference = "continue"
+while($true)
+{
+$entry = Get-Content  $env:USERPROFILE\AppData\Roaming\PepeCoin\debug.log | select -Last 1
+if($entry|Select-String -Pattern 'ERROR')
+    {
+    Write-Verbose -Message "Error Detected in Log!"
+    Send-TestEmail -To <enter email> -Cc <enter CC email> -body $entry
+    Start-Sleep -s 10800
+    }
+
+}
